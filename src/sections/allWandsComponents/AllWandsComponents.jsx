1878056e14fa0c1f3562074c802f28c80b04d82f
Phillipml <contato.phillip.menezes@gmail.com>
2024-02-22 19:52:38 -0300

    #07 images from Wand Components are created

--- src/sections/allWandsComponents/AllWandsComponents.jsx
+++ src/sections/allWandsComponents/AllWandsComponents.jsx
@@ -1,4 +1,9 @@
+import wand1 from "../../assets/wandComponents/wand/wand1.png";
 function AllWandsComponents() {
-  return <div>WandsComponents</div>;
+  return (
+    <div>
+      <img width="50px" src={wand1} />
+    </div>
+  );
 }
 export default AllWandsComponents;
